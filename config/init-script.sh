72a0c56a56a3436eb1d175e7fbf21a9c8fe48a0f
RushLana <renarde-dev@la-taniere-solidaire.fr>
2026-03-24 10:14:05 +0100

    Add a UID/GID system to allow user ownership of the piwigo folder (#14)
    Internally use ACLs and get UID/GID from .env
    Add fallback for UID/GID .env which default to 1000:1000

--- config/init-script.sh
+++ config/init-script.sh
@@ -4,6 +4,8 @@ set -eu
 
 TZVAL="${TZ}"
 PHPV="${PHP_VERSION}"
+PIWIGO_USER_ID="${PIWIGO_UID:-1000}"
+PIWIGO_GROUP_ID="${PIWIGO_GID:-1000}"
 
 ## Set Timezone
 # check the timezone in /usr/share/zoneinfo and fallback to UTC if it doesn't exist
@@ -19,10 +21,6 @@ echo "$TZVAL" > /etc/timezone
 echo "date.timezone=${TZVAL}" > "/etc/php${PHPV}/conf.d/99-timezone.ini"
 
 echo "[timezone] set to '${TZVAL}'" >&2
-
-
-## Ensure directories are writable (see https://github.com/MariaDB/mariadb-docker/blob/master/docker-entrypoint.sh)
-find "/var/www/html/piwigo/" \! -user nginx \( -exec chown nginx: '{}' + -o -true \)
 
 SOURCE_VERSION=$(php$PHPV -r "include '/var/www/source/piwigo/include/constants.php'; echo PHPWG_VERSION;" 2> /dev/null)
 if [ -f '/var/www/html/piwigo/include/constants.php' ]; then
@@ -40,6 +38,10 @@ else
     echo "Installing piwigo $SOURCE_VERSION"
     /bin/cp -arT /var/www/source/piwigo /var/www/html/piwigo/
 fi
+
+## Ensure directories are readable and writable by nginx and the user with ACLs intenally and Unix ownership externally 
+setfacl -R -m u:nginx:rwx /var/www/html/piwigo
+find "/var/www/html/piwigo/" \( ! -user $PIWIGO_USER_ID -o ! -group $PIWIGO_GROUP_ID \) -exec chown $PIWIGO_USER_ID:$PIWIGO_GROUP_ID '{}' \;
 
 ## Load user scripts if it exist
 if [ -e "/usr/local/bin/scripts/user.sh" ]; then
