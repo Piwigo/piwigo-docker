3f8851fb684a5203ff11dd0cbbbad8fba607b117
RushLana <renarde-dev@la-taniere-solidaire.fr>
2026-03-27 10:36:02 +0100

    Workaround for non bind-mount system like MacOS and Windows
    Default to chmod o+rwx when the first setfacl command fails.
    This should not change anything for non bind-mount users.

--- config/init-script.sh
+++ config/init-script.sh
@@ -40,10 +40,11 @@ else
 fi
 
 ## Ensure directories are readable and writable by nginx and the user with ACLs intenally and Unix ownership externally
+## Default to chmod o+rwx in non bind-mount scenario
 if setfacl -m u:nginx:rwx /var/www/html/piwigo ; then
     setfacl -R -m u:nginx:rwx /var/www/html/piwigo
 else
-    echo "Non bind-mount falling back to chmod o+rwx"
+    echo "Non-bind-mount environment falling back to chmod o+rwx (expected on macOS and Windows, you can safely ignore setfacls warnings)"
     chmod o+rwx /var/www/html/piwigo 
 fi
 find "/var/www/html/piwigo/" \( ! -user $PIWIGO_USER_ID -o ! -group $PIWIGO_GROUP_ID \) -exec chown $PIWIGO_USER_ID:$PIWIGO_GROUP_ID '{}' \;
